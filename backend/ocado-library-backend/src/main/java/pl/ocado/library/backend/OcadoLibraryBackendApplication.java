17f086e0f1d52002527e5515ffe125d38304da43
MDek <mikhaildek24@gmail.com>
2026-03-10 21:38:12 +0100

    feat: add backend setup and CI workflow for app build

---
+++ backend/ocado-library-backend/src/main/java/pl/ocado/library/backend/OcadoLibraryBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.ocado.library.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcadoLibraryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OcadoLibraryBackendApplication.class, args);
+	}
+
+}
