2a6f3a727dcc6b47903a0f8e30a68483fd5261f2
jbie280949 <280949@student.pwr.edu.pl>
2026-03-14 21:45:52 +0100

    Created entities and local PostgreSQL database setup.

--- backend/ocado-library-backend/src/main/java/pl/ocado/library/backend/OcadoLibraryBackendApplication.java
+++ backend/ocado-library-backend/src/main/java/pl/ocado/library/backend/OcadoLibraryBackendApplication.java
@@ -3,6 +3,11 @@ package pl.ocado.library.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+// On its own the app won't start. You need to change your postgres password in resources/application.properties
+// and create your local ocado_library_db database in order to run it correctly. Or just uncomment the h2 setup
+// and comment the postgres one.
+
 @SpringBootApplication
 public class OcadoLibraryBackendApplication {
 
---
+++ backend/ocado-library-backend/src/main/java/pl/ocado/library/backend/domain/entities/BoardGame.java
@@ -0,0 +1,31 @@
+package pl.ocado.library.backend.domain.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "board_games")
+public class BoardGame {
+
+    @Id
+    private String id;
+    private String name;
+    @Column(name = "link_board_game_geek")
+    private String linkBoardGameGeek;
+    @Column(name = "number_of_players")
+    private String numberOfPlayers;
+    private String borrower;
+    @Column(name = "borrowing_date")
+    private LocalDate borrowingDate;
+
+}
