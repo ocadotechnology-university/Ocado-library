e23627fcda9d192fc4c9f88f031930c657a3a98d
jbie280949 <280949@student.pwr.edu.pl>
2026-04-14 21:54:35 +0200

    Feat/fix: Major changes in database. Added junction tables and reorganized tables structure.

--- database/init.sql
+++ database/init.sql
@@ -1,14 +1,45 @@
--- Tabela dla książek
-CREATE TABLE books (
-    id SERIAL PRIMARY KEY,
+-- Metadane książek (opis)
+CREATE TABLE book_descriptions (
+    id SERIAL PRIMARY KEY, -- ID łączące opis książki z fizycznym obiektem
     isbn VARCHAR(20) UNIQUE,
     title VARCHAR(255) NOT NULL,
-    author VARCHAR(255) NOT NULL,
-    edition VARCHAR(50),
-    status VARCHAR(50),
+    author VARCHAR(255) NOT NULL
+);
+
+-- Tabela dla książek (jako obiekty)
+CREATE TABLE books (
+    id SERIAL PRIMARY KEY, -- Wewnętrzne ID dla bazy danych (raczej nie będzie używane)
+    inventory_code VARCHAR(50) UNIQUE NOT NULL, -- ID przypisywane jako OC-WR-B-xxxx
+    status VARCHAR(50) DEFAULT  'AVAILABLE', -- Status domyślny jako AVAILABLE
+    book_description_id INT NOT NULL, -- Referencja do metadanych książki (autor, opis, kategoria, itd...)
     borrower VARCHAR(255),
-    category VARCHAR(100),
-    borrowing_date DATE
+    borrowing_date DATE,
+
+    FOREIGN KEY (book_description_id) REFERENCES book_descriptions(id)
+);
+
+-- Tabela dla tagów
+CREATE TABLE tags (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) UNIQUE NOT NULL
+);
+
+-- Tabela łącząca tagi z książkami
+CREATE TABLE book_tags (
+    book_description_id INT NOT NULL,
+    tag_id INT NOT NULL,
+    PRIMARY KEY (book_description_id, tag_id), -- Primary Key zapewnia unikalność tagów dla danej książki
+    FOREIGN KEY (book_description_id) REFERENCES book_descriptions(id) ON DELETE CASCADE, -- Foreign Key z delete on cascade zapewnia integralność danych i eliminuje 'ghost data' przy usuwaniu
+    FOREIGN KEY (tag_id) REFERENCES tags(id) ON DELETE CASCADE -- Podobnie jak powyżej, dane są usuwane równolegle
+);
+
+-- Tabela do oczekiwania na książkę
+CREATE TABLE book_waitlist (
+    id SERIAL PRIMARY KEY,
+    book_description_id INT NOT NULL, -- Na jaką książkę oczekujemy
+    waiter_name VARCHAR(255) NOT NULL, -- Kto oczekuje
+    joined_queue_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- Kto wcześniej zaczął czekać
+    FOREIGN KEY (book_description_id) REFERENCES book_descriptions(id) ON DELETE CASCADE -- Integralność przy usuwaniu
 );
 
 -- Tabela dla gier planszowych
@@ -29,9 +60,3 @@ CREATE TABLE ps5_games (
     name VARCHAR(255) NOT NULL,
     status VARCHAR(50)
 );
-
--- Tabela dla tagów
-CREATE TABLE tags (
-    id INT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL
-)
