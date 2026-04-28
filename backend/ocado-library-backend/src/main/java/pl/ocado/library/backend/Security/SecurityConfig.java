dd47c0a9dae4ba57d0cb22ccce4858a300cd5f85
MDek <mikhaildek24@gmail.com>
2026-04-28 21:04:26 +0200

    fix: Conflicts resolved

--- backend/ocado-library-backend/src/main/java/pl/ocado/library/backend/Security/SecurityConfig.java
+++ backend/ocado-library-backend/src/main/java/pl/ocado/library/backend/Security/SecurityConfig.java
@@ -109,7 +109,6 @@ public class SecurityConfig {
                 .headers(headers -> headers.frameOptions(frame -> frame.disable()))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html").permitAll()
-                        .requestMatchers("/h2-console/**").permitAll()
                         .requestMatchers("/oauth2/**", "/login/oauth2/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/**").hasAnyRole("USER", "ADMIN")
                         .requestMatchers(HttpMethod.POST, "/api/**").hasRole("ADMIN")
